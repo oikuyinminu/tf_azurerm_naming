d8e3d1056b7711d300faa07f103b5d3a27f733ee
Banji IKUYINMINU <i.banji@hotmail.com>
2022-03-12 09:53:22 +1100

    create naming conventions

---
+++ locals.tf
@@ -0,0 +1,22 @@
+locals {
+
+  loc = {
+    eastus  = "eus1"
+    eastus2 = "eus2"
+  }
+
+  env = {
+    "production" : "prd"
+    "preproduction" : "ppd"
+    "development" : "dev"
+    "test" : "tst"
+  }
+
+
+  # Generate names for the resources
+  prj = lower(substr(var.project, 0, 3))
+  solution = lower(substr(var.solution, 0, 3))
+  std = format("%s-%s-%s", lookup(local.loc, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
+  vm_name = local.solution != "" ? format("vm%s%s%s%s", lookup(local.loc, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj, local.solution) : format("vm%s%s%s", lookup(local.loc, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
+
+}
---
+++ outputs.tf
@@ -0,0 +1,55 @@
+output "rg" {
+    value = local.solution != "" ? format("rg-%s-%s", local.std, local.solution) : format("rg-%s", local.std)
+}
+
+output "vnet" {
+    value = format("vnet-%s-%s", lookup(local.loc, var.location), lookup(local.env, replace(var.environment, "-", "")))
+}
+
+output "subnets" {
+    value = {
+        for x in var.subnets :
+        lower(x) => format("snet-%s-%s-%s", lookup(local.loc, var.location), lookup(local.env, replace(var.environment, "-", "")), lower(x))
+    }
+}
+
+output "nsg" {
+    value = format("nsg-%s-%s", lookup(local.loc, var.location), lookup(local.env, replace(var.environment, "-", "")))
+}
+
+output "vm" {
+    value = local.vm_name
+}
+
+output "datadisk" {
+    value = "stvm-${local.vm_name}"
+}
+
+output "disk" {
+    value = "stvm-${local.vm_name}"
+}
+
+output "nic" {
+    value = "nic-${local.vm_name}"
+}
+
+output "pip" {
+    value = local.solution != "" ? format("pip-%s-%s", local.std, local.solution) : format("pip-%s", local.std)  
+}
+
+output "pep" {
+    value = local.solution != "" ? format("pep-%s-%s", local.std, local.solution) : format("pep-%s", local.std)
+}
+
+output "law" {
+    value = local.solution != "" ? format("law-%s-%s", local.std, local.solution) : format("law-%s", local.std)
+}
+
+output "rsv" {
+    value = local.solution != "" ? format("rsv-%s-%s", local.std, local.solution) : format("rsv-%s", local.std)  
+}
+
+output "autoacct" {
+    value = local.solution != "" ? format("autoacct-%s-%s", local.std, local.solution) : format("autoacct-%s", local.std)
+}
+
