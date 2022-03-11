64b720f5dea1528552b2a6d4e64b468a46f417c2
Banji IKUYINMINU <i.banji@hotmail.com>
2022-03-12 10:07:55 +1100

    create naming conventions

--- locals.tf
+++ locals.tf
@@ -14,9 +14,9 @@ locals {
 
 
   # Generate names for the resources
-  prj = lower(substr(var.project, 0, 3))
+  prj      = lower(substr(var.project, 0, 3))
   solution = lower(substr(var.solution, 0, 3))
-  std = format("%s-%s-%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
-  vm_name = local.solution != "" ? format("vm%s%s%s%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj, local.solution) : format("vm%s%s%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
+  std      = format("%s-%s-%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
+  vm_name  = local.solution != "" ? format("vm%s%s%s%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj, local.solution) : format("vm%s%s%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
 
 }
--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable "location" {
-    description = "(Required) The region that resources are deployed."
-    type = string 
+  description = "(Required) The region that resources are deployed."
+  type        = string
 }
 
 variable "environment" {
-    description = "(Required) The environment resources are deployed to."
-    type = string
+  description = "(Required) The environment resources are deployed to."
+  type        = string
 }
 
 variable "project" {
-    description = "(Required) A 3 Character representation of the project."
-    typr = string
+  description = "(Required) A 3 Character representation of the project."
+  typr        = string
 }
 
 variable "solution" {
   description = "(Optional) An application or part of the project"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "subnets" {
-    description = "(Optional) List of subnet names to be created"
-    type = list(string)
-    default = []
+  description = "(Optional) List of subnet names to be created"
+  type        = list(string)
+  default     = []
 }
