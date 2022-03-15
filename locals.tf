c71a8a7c56975b32df3f577d83e7e72eacf79ff4
Banji IKUYINMINU <i.banji@hotmail.com>
2022-03-15 14:35:10 +1100

    env values

--- locals.tf
+++ locals.tf
@@ -18,8 +18,8 @@ locals {
   solution    = lower(substr(var.solution, 0, 3))
   std         = format("%s-%s-%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
   std_noloc   = format("%s-%s", lookup(local.env, replace(var.environment, "-", "")), local.prj)
-  stdsql      = format("%s-%s-%s-%s", lookup(local.reg, var.location), lookup(loca.env, replace(var.environment, "-", "")), local.solution, local.prj)
-  std_no_dash = format("%s%s%s", lookup(local.ref, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
+  stdsql      = format("%s-%s-%s-%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.solution, local.prj)
+  std_no_dash = format("%s%s%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
   vm_name     = local.solution != "" ? format("vm%s%s%s%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj, local.solution) : format("vm%s%s%s", lookup(local.reg, var.location), lookup(local.env, replace(var.environment, "-", "")), local.prj)
 
 }
